3fd75914340145783a0a0b81bb9a7df4db5d547e
kkats <k.katsumata@jamstec.go.jp>
2019-11-18 14:42:45 +0900

    Wrong A05/batch.sh

--- A05/batch.sh
+++ A05/batch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-DIR=A03
-name=a03
+DIR=A05
+name=a05
 (cd ../output/gridded/$DIR; gzip *xyz)
-for year in 1993
+for year in 1992 1998 2004 2010 2011 2015
 do
     (cd ../output/reported/work; zip ../$DIR/${name}_${year}_ct1.zip ${name}_${year}_????_ct1.csv; rm -fr ${name}_${year}_????_ct1.csv)
 done
