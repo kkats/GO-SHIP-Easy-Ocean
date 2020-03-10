bff524e4e438e9cbc7b0f0d815482349b832ccb5
kkats <k.katsumata@jamstec.go.jp>
2020-03-10 12:21:57 +0900

    P17 added

---
+++ P17/configuration_1991.m
@@ -0,0 +1,9 @@
+% P17, 1991
+vinterp_handle = @vinterp;
+hinterp_handle = @hinterp;
+MAX_SEPARATION = 2.0;
+
+salt_offset([1:141]) = 1.0e-3 * 2.0; % P114 (31WTTUNES_1, 31WTTUNES_2(124-140))
+salt_offset([142:221]) = 1.0e-3 * 1.7; % P108 31WTTUNES_2 (141-220)
+salt_offset([222:455]) = 1.0e-3 * (-0.9); % P120 316N138_9, 316N138_10
+salt_offset([456:658]) = 1.0e-3 * 0.4; % P122 325021_2
