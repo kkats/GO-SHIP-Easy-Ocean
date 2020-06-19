24f53c5024495c95ef03077c1361641eaa54585a
kkats <k.katsumata@jamstec.go.jp>
2020-06-19 09:17:24 +0900

    Remove residuals from batch jobs

--- batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DIR=P11
-name=p11
-(cd ../output/gridded/$DIR; gzip *xyz)
-for year in 1993
-do
-    (cd ../output/reported/work; zip ../$DIR/${name}_${year}_ct1.zip ${name}_${year}_????_ct1.csv; rm -fr ${name}_${year}_????_ct1.csv)
-done
