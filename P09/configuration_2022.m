03b069c2fec2753a75fc94427ffad16a86362074
kkats <k.katsumata@jamstec.go.jp>
2023-09-15 11:44:04 +0900

    Found SSW batch for P09(2022)

--- P09/configuration_2022.m
+++ P09/configuration_2022.m
@@ -3,4 +3,4 @@ vinterp_handle = @vinterp_gauss;
 hinterp_handle = @hinterp_bylat;
 MAX_SEPARATION = 2.0;
 
-salt_offset([1:93]) = 1.0e-3 * 0.0; % unknown (49UP20220727) emailed the PI but no reply
+salt_offset([1:93]) = 1.0e-3 * (-0.1); % P165 (49UP20220727, personal communication but to be documented soon).
