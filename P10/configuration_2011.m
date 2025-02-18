f83739ad62217d804d8709fbe8934d226446f200
kkats <k.katsumata@jamstec.go.jp>
2025-02-18 09:16:09 +0900

    P10 (2011) The cruise 49NZ20111220 was inadvertently missing.

--- P10/configuration_2011.m
+++ P10/configuration_2011.m
@@ -3,4 +3,4 @@ vinterp_handle = @vinterp_gauss;
 hinterp_handle = @hinterp_bylat;
 MAX_SEPARATION = 2.0;
 
-salt_offset([1:60]) = 1.0e-3 * 0.4; % P153 (49NZ20111220/49NZ20120113)
+salt_offset([1:126]) = 1.0e-3 * 0.4; % P153 (49NZ20111220/49NZ20120113)
